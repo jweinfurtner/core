10efcdc1a500fe04e0974421f36c92801f775788
jweinfurtner <jweinfurtner+github@gmail.com>
2013-11-01 19:25:39 +0100

    webmvc functioning

--- core/src/main/java/com/jweinfurtner/core/config/WebConfig.java
+++ core/src/main/java/com/jweinfurtner/core/config/WebConfig.java
@@ -1,9 +1,12 @@
 package com.jweinfurtner.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.jweinfurtner.core.controller.HomeController;
 
@@ -12,4 +15,13 @@ import com.jweinfurtner.core.controller.HomeController;
 @ComponentScan(basePackageClasses = { WebConfig.class, HomeController.class })
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Bean
+	public ViewResolver getViewResolver() {
+
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
 }
--- core/src/main/java/com/jweinfurtner/core/controller/HomeController.java
+++ core/src/main/java/com/jweinfurtner/core/controller/HomeController.java
@@ -15,25 +15,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
+@RequestMapping(value = "/")
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
+		System.out.println("here!");
+
 		return "home";
 	}
-	
+
 }
