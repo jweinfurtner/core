274c0978740ce721c8cc4905ce869abff15d5788
jweinfurtner <jweinfurtner+github@gmail.com>
2013-11-01 18:06:02 +0100

    refactoring main to core

--- core/src/main/java/com/jweinfurtner/core/config/MongoDBConfig.java
+++ core/src/main/java/com/jweinfurtner/core/config/MongoDBConfig.java
@@ -1,4 +1,4 @@
-package com.jweinfurtner.main.config;
+package com.jweinfurtner.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- core/src/main/java/com/jweinfurtner/core/config/WebSecurityConfig.java
+++ core/src/main/java/com/jweinfurtner/core/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.jweinfurtner.main.config;
+package com.jweinfurtner.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
