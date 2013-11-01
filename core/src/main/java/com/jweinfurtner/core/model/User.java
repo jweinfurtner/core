b53434c421cc7317cd12d4110980e042dff08161
jweinfurtner <jweinfurtner+github@gmail.com>
2013-11-01 19:55:07 +0100

    user modelling

---
+++ core/src/main/java/com/jweinfurtner/core/model/User.java
@@ -0,0 +1,39 @@
+package com.jweinfurtner.core.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+	@Id
+	private String id;
+
+	String email;
+
+	String password;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
